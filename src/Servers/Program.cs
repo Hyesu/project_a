3acb26fcc4263eb0056b65cfde7ae48a691d1402
hotspam <accrmfrn84@gmail.com>
2023-11-12 23:52:41 +0900

    라이더 연동 및 유니티에 newtonsoft package 설치

--- src/Servers/Program.cs
+++ src/Servers/Program.cs
@@ -1,37 +1,29 @@
 ﻿using System.Configuration;
 using DesignTable.Core;
 
-internal class Program
+Console.WriteLine("==================== Start Server ====================");
+
+// TODO: app config가 아닌 별도 파일 통해서 setting 설정할 수 있도록 수정 필요
+// load design table
+var ddRoot = ConfigurationManager.AppSettings["DesignDataRoot"];
+if (null == ddRoot)
+    throw new InvalidDataException($"not found app setting - DesignDataRoot");
+
+var dt = new DContext(ddRoot);
+dt.Initialize();
+
+// test code
+foreach (var section in dt.Sections)
 {
-    private static void Main(string[] args)
+    foreach (var entry in section.All)
     {
-        Console.WriteLine("==================== Start Server ====================");
-
-        var cwd = Directory.GetCurrentDirectory();
-
-        // TODO: 서버 클래스 만들 때 여기서 setting만 넘겨주고, 서버 내부에서 초기화하게 수정
-        // load encyclopedia
-        var ddRoot = ConfigurationManager.AppSettings["DesignDataRoot"];
-        if (null == ddRoot)
-            throw new InvalidDataException($"not found app setting - DesginDataRoot");
-
-        var dt = new DContext(ddRoot);
-        dt.Initialize();
-
-        // test code
-        foreach (var section in dt.Sections)
-        {
-            foreach (var entry in section.All)
-            {
-                Console.WriteLine($"table({section.Name}) - id({entry.Id}) strId({entry.StrId})");
-            }
-        }
-
-        var dlg = dt.Dialog.GetByStrId("dlg_sample");
-        Console.WriteLine($"dlg({dlg.StrId}) type({dlg.Type})");
-        foreach (var speech in dlg.Speeches)
-        {
-            Console.WriteLine($"-- {speech.Character} : {speech.Text} // emotion({speech.Emotion})");
-        }
+        Console.WriteLine($"table({section.Name}) - id({entry.Id}) strId({entry.StrId})");
     }
+}
+
+var dlg = dt.Dialog.GetByStrId("dlg_sample");
+Console.WriteLine($"dlg({dlg.StrId}) type({dlg.Type})");
+foreach (var speech in dlg.Speeches)
+{
+    Console.WriteLine($"-- {speech.Character} : {speech.Text} // emotion({speech.Emotion})");
 }
