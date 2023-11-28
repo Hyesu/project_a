d04da93331c304fdef07b3fb3e7b54b3cfe3c8f1
hotspam <accrmfrn84@gmail.com>
2023-11-28 22:30:30 +0900

    공용 파일은 별도 프로젝트로 분리

--- src/Feature/Dialog/IDialogHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Feature.Dialog;
-
-public interface IDialogHandler
-{
-    void OnStarted(DialogContext ctx);
-    void OnUpdated(DialogContext ctx);
-    void OnEnded(DialogContext ctx);
-}
