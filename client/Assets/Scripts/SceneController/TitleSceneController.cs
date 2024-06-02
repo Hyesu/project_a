3811f38334bfc4e5cf580c39d4e29f0a733492ce
Hyesu <accrmfrn84@gmail.com>
2024-06-02 19:54:47 +0900

    3d project로 변환 & 타이틀씬

---
+++ client/Assets/Scripts/SceneController/TitleSceneController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleSceneController : MonoBehaviour
+{
+    public void OnClickedStartButton()
+    {
+        Debug.Log($"clicked start button");
+    }
+}
